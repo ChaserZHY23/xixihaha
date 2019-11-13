15003b9d761af25dd0190c968968c834d62fdf1b
ChaserZHY23 <804766877@qq.com>
2019-11-13 22:45:03 +0800

    daily practice

---
+++ 20191112/20191112/Program.cs
@@ -0,0 +1,44 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20191112
+{
+    interface IEnglishDimensions
+    {
+        float Length();
+        float Width();
+    }
+    interface IMetricDimensions
+    {
+        float Legth();
+        float Width();
+    }
+    class Box : IEnglishDimensions, IMetricDimensions
+    {
+        float lengthInches;
+        float WidthInches;
+        public Box(float length; float width)
+     {  
+            lengthInches=length;
+            widthInches=width;
+      }
+    float IE
+
+
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ 20191112b/20191112b/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20191112b
+{
+    interface IEnglishDimensions
+    {
+        float Length();
+        float Width();
+    }
+    interface IMetricDimensions
+    {
+        float Legth();
+        float Width();
+    }
+   
+    class Box : IEnglishDimensions, IMetricDimensions
+    {
+        
+            float lengthInches;
+            float WidthInches;
+            public Box(float length; float width)
+     {  
+            lengthInches=length;
+            widthInches=width;
+      }
+    float IEnglishDimensions
+        static void Main(string[] args)
+        {
+        }
+    }
+}
